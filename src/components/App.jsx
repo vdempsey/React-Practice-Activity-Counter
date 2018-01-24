ab1e2496960f329338d19d3e0d22e3a1103892ee
Valia Dempsey <valentinaml@gmail.com>
2018-01-24 09:45:58 -0800

    Edit App Component: add GetInitialState to initialize state as the default, return an object with the key allSkiDays. Enter data from state to display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
+import { createClass } from 'react';
 import SkiDayList from './SkiDayList';
 import '../stylesheets/ui.scss';
 
-function App(){
-  return (
-    <div>
-      <SkiDayList days={
-        [
-          {
-            resort: 'Squaw Valley',
-            date: new Date('1/2/2016'),
-            powder: true,
-            backcountry: false
-          },
-          {
-            resort: 'Kirkwood',
-            date: new Date('3/28/2016'),
-            powder: false,
-            backcountry: false
-          },
-          {
-            resort: 'Mt. Tallac',
-            date: new Date('4/2/2016'),
-            powder: false,
-            backcountry: true
-          }
-        ]
-      }/>
-    </div>
-  );
-}
+const App = createClass({
+	getInitialState() {
+		return {
+			allSkiDays: [
+			{
+				resort: "Squaw Valley",
+				date: new Date("1/2/2016"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Kirkwood",
+				date: new Date("3/28/2016"),
+				powder: false,
+				backcountry: false
+			},
+			{
+				resort: "Mt. Tallac",
+				date: new Date("4/2/2016"),
+				powder: false,
+				backcountry: true
+			}
+		]
+		}
+	},
+	render() {
+		return (
+			<div className="app">
+				{this.state.allSkiDays[0]["resort"]}
+			</div>
+		)
+	}
+})
 
 export default App;

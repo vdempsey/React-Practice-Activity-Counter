024d66daa17b409c2966322754e8e876c36f66ec
Valia Dempsey <valentinaml@gmail.com>
2018-01-23 21:57:27 -0800

    Create SkyDayCount Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import TestListComponent from './TestListComponent';
 import TestHeaderComponent from './TestHeaderComponent';
+import SkiDayCount from './SkiDayCount';
+import '../stylesheets/ui.scss';
 
 function App(){
   return (
     <div>
-      <TestHeaderComponent/>
-      <TestListComponent />
+      <SkiDayCount/>
     </div>
   );
 }
--- src/components/SkiDayCount.jsx
+++ src/components/SkiDayCount.jsx
@@ -1,1 +1,22 @@
 import React from 'react';
+import '../stylesheets/ui.scss';
+
+class SkiDayCount extends React.Component {
+  render() {
+    return (
+      <div className='ski-day-count'>
+        <div className='total-days'>
+          <span>5 days</span>
+        </div>
+        <div className='powder-days'>
+          <span>2 days</span>
+        </div>
+        <div className='backcountry-days'>
+          <span>1 hiking day</span>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SkiDayCount;

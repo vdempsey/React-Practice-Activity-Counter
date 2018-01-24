f9946829a6af6aa29851bcf6e5e632f84824a648
Valia Dempsey <valentinaml@gmail.com>
2018-01-23 22:35:14 -0800

    Install react icon library. Add icons to SkiDayCount Component

--- src/components/SkiDayCount.jsx
+++ src/components/SkiDayCount.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../stylesheets/ui.scss';
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
 const percentToDecimal = (decimal) => {
   return ((decimal * 100) + '%')
@@ -13,14 +16,17 @@ const SkiDayCount = ({total, powder, backcountry, goal}) => (
   <div className='ski-day-count'>
     <div className='total-days'>
       <span>{total}</span>
+        <Calendar />
       <span>days</span>
     </div>
     <div className='powder-days'>
       <span>{powder}</span>
+        <SnowFlake />
       <span>days</span>
     </div>
     <div className='backcountry-days'>
       <span>{backcountry}</span>
+        <Terrain />
       <span>days</span>
     </div>
     <div>
